3135931347df11e58cb2732eaf130bf93a80fbbb
Jared Bangs <jared@agilestory.com>
2016-01-30 14:52:53 -0800

    web stuff

--- web/web.go
+++ web/web.go
@@ -19,6 +19,7 @@ func (w *Web) Start() {
 	w.initializeRepo()
 
 	http.HandleFunc("/", w.handleListFeedsRequest)
+	http.HandleFunc("/show/", w.showFeed)
 
 	log.Println("Listening on :8080")
 	http.ListenAndServe(":8080", nil)
@@ -39,5 +40,17 @@ func (w *Web) initializeRepo() {
 
 	if w.repo == nil {
 		w.repo = repositories.NewFeedRepository(w.Config)
+	}
+}
+
+func (w *Web) showFeed(rw http.ResponseWriter, r *http.Request) {
+
+	t, _ := template.ParseFiles("web/showFeed.html")
+
+	id := r.URL.Path[len("/show/"):]
+
+	feed, err := w.repo.ReadById(id)
+	if err == nil {
+		t.Execute(rw, feed)
 	}
 }
